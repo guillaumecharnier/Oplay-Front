ee38dd6c6f1cbe400a6a778de2bc861f9b239e34
VictorLuigi27 <victor.diaz@oclock.school>
2024-07-02 14:36:21 +0200

    Changement couleurs HomePage

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import ModalProfil from '../ModalProfil/ModalProfil';
 
 interface HeaderProps {
--- src/components/ModalProfil/ModalProfil.tsx
+++ src/components/ModalProfil/ModalProfil.tsx
@@ -1,21 +1,37 @@
 function ModalProfil({ closeModal }) {
   //TODO typer closeModal
   return (
-    <div className="fixed vh-100 bg-slate-500 w-full h-full max-w-screen-lg">
-      <span className="absolute right-3" onClick={closeModal}>
+    <div className="fixed top-0 h-screen w-full bg-slate-300 z-10 flex flex-col items-center justify-center ">
+      <span className="absolute text-4xl top-5 right-10 " onClick={closeModal}>
         x
       </span>
-      <img src="src/assets/images/profile-user.svg" alt="image de profil" />
+      <div className="absolute top-0">
+        <img
+          src="src/assets/images/gamepad.svg"
+          alt="Logo page accueil"
+          className="w-8"
+        />
+        <h1 className="text-xl ml-2">O'Play</h1>
+      </div>
+      <img src="src/assets/images/profile-user.svg" alt="image de profil" className="w-24"/>
       <a href="">
-        <h3>Profil</h3>
+        <h3 className="text-4xl">Profil</h3>
       </a>
       <a href="">
-        <h3>Parametre</h3>
+        <h3 className="text-4xl">Parametre</h3>
       </a>
       <a href="">
-        <h3>Whishlist</h3>
+        <h3 className="text-4xl">Whishlist</h3>
       </a>
-      <h3>Deconnexion</h3>
+      <h3 className="text-4xl">Deconnexion</h3>
+      <div className="flex flex-row h-8 w-60 justify-between">
+        <span className="rounded-full w-8 border-2 border-solid border-black bg-red-500"></span>
+        <span className="rounded-full w-8 border-2 border-solid border-black bg-blue-500"></span>
+        <span className="rounded-full w-8 border-2 border-solid border-black bg-orange-500"></span>
+        <span className="rounded-full w-8 border-2 border-solid border-black bg-green-500"></span>
+        <span className="rounded-full w-8 border-2 border-solid border-black bg-purple-500"></span>
+        <span className="rounded-full w-8 border-2 border-solid border-black bg-yellow-500"></span>
+      </div>
     </div>
   );
 }
