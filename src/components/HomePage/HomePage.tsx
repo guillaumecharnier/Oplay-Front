1a62430fc1e25e2865304ec85ea2ea9cad187f7e
VictorLuigi27 <victor.diaz@oclock.school>
2024-07-02 14:42:31 +0200

    Erreur

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,47 +1,44 @@
-// HomePage.tsx
 import React from 'react';
 import CustomSelection from './CustomSelection';
 import LastAdditions from './LastAdditions';
 import Categories from './Categories';
 import NextRelease from './NextRelease';
 
-
 function HomePage() {
   return (
-    <div className="bg-blue-950 min-h-screen flex flex-col items-center justify-center p-4 overflow-x-hidden">
+    <div className="bg-blue-200 min-h-screen flex flex-col items-center justify-start overflow-x-hidden">
       
-    
       {/* Image de couverture */}
-      <div className="relative w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mb-6 flex justify-center">
+      <div className="relative w-full mb-36 flex justify-center">
         <img
-          src="/images/Last-Of-Us.jpeg"
+          src="src/assets/images/Last-Of-Us.jpeg"
           alt="The Last of Us"
-          className="w-72 sm:w-56 md:w-64 lg:w-72 max-h-80 h-auto object-cover rounded-lg shadow-lg"
+          className="w-full max-h-80 object-cover shadow-lg"
         />
-        <div className="absolute top-2 left-8">
-          <p className="text-white text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
+        <div className="absolute top-4 md:left-6 lg:left-8">
+          <p className="text-white text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
             The Last Of Us Part I - $39.99
           </p>
         </div>
       </div>
 
       {/* Section CustomSelection */}
-      <div className="mb-16 md:mb-24">
+      <div className="w-full max-w-screen-lg mb-16 md:mb-24 lg:mb-28 xl:mb-32">
         <CustomSelection />
       </div>
 
       {/* Section LastAdditions */}
-      <div className="mb-16 md:mb-24">
+      <div className="w-full max-w-screen-lg mb-16 md:mb-24 lg:mb-28 xl:mb-32">
         <LastAdditions />
       </div>
 
       {/* Section NextRelease */}
-      <div className="mb-16 md:mb-24">
+      <div className="w-full max-w-screen-lg mb-16 md:mb-24 lg:mb-28 xl:mb-32">
         <NextRelease />
       </div>
 
       {/* Section Categories */}
-      <div className="mb-16 md:mb-24">
+      <div className="w-full max-w-screen-lg mb-16 md:mb-24 lg:mb-28 xl:mb-32">
         <Categories />
       </div>
     </div>
@@ -49,5 +46,7 @@ function HomePage() {
 }
 
 export default HomePage;
+
+
 
 
