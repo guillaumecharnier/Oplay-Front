dad37dded5f4ece59162bfd614b11caff62c0fa5
VictorLuigi27 <victor.diaz@oclock.school>
2024-07-12 14:45:04 +0200

    Modif Param Design

--- src/components/Parametre/Parametre.tsx
+++ src/components/Parametre/Parametre.tsx
@@ -6,21 +6,20 @@ function Parametre() {
   const { theme } = useTheme(); // Récupération du thème
 
   return (
-    <div className="p-10 flex">
-      <div className={`min-h-screen ${theme} p-10 flex rounded-md bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500  w-1/2 `}>
-        <ul className="text-white leading-[5rem] pl-5">
-          <li className=" rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black md:border-b-2">
-          <Link to="/test-personnalie">Test personnalisé</Link>
+    <div className="p-10 flex justify-center items-center h-screen">
+      <div className={`h-3/4 ${theme} p-10 flex rounded-md bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 w-1/2`}>
+        <ul className="text-white w-full space-y-2 flex flex-col justify-between h-full">
+          <li className="rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black text-base">
+            <Link to="/test-personnalite">Test personnalisé</Link>
           </li>
-          <li className=" rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black md:border-b-2">Langue/ devise</li>
-          <li className=" rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black md:border-b-2">Historique</li>
-          <li className=" rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black md:border-b-2">Facturation</li>
-          <li className=" rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black md:border-b-2">Confidentialités</li>
-          <li className=" rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black md:border-b-2">Notification</li>
-          <li className=" rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black">Mail /Mot de passe</li>
+          <li className="rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black text-base">Langue/ devise</li>
+          <li className="rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black text-base">Historique</li>
+          <li className="rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black text-base">Facturation</li>
+          <li className="rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black text-base">Confidentialités</li>
+          <li className="rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black text-base">Notification</li>
+          <li className="rounded-md hover:bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 pl-2 hover:text-black text-base">Mail /Mot de passe</li>
         </ul>
       </div>
-      {/* <div className="bg-red-500 w-1/2"></div> */}
     </div>
   );
 }
