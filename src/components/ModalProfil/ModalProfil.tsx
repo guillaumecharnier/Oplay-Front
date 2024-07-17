c6139320d6772b98400e8d751be0d50c349914ee
VictorLuigi27 <victor.diaz@oclock.school>
2024-07-17 09:24:13 +0200

    Modif

--- src/components/ModalProfil/ModalProfil.tsx
+++ src/components/ModalProfil/ModalProfil.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import { useTheme } from "../../context/ThemeContext";
@@ -9,7 +9,7 @@ interface ModalProfilProps {
 
 const ModalProfil: React.FC<ModalProfilProps> = ({ closeModal }) => {
   const { isLog, roles, logout } = useAuth();
-  const isAdmin = roles && roles.includes('ROLE_ADMIN');
+  const [isAdmin, setIsAdmin] = useState(false);
   const { postThemeData } = useTheme();
 
   const themes = [
@@ -21,6 +21,57 @@ const ModalProfil: React.FC<ModalProfilProps> = ({ closeModal }) => {
     { id: 6, name: 'Sport', color: 'bg-yellow-600' },
   ];
 
+  useEffect(() => {
+    console.log('Roles updated:', roles);
+    if (roles && roles.includes('ROLE_ADMIN')) {
+      setIsAdmin(true);
+    } else {
+      setIsAdmin(false);
+    }
+  }, [roles]);
+
+  useEffect(() => {
+    console.log('isLog updated:', isLog);
+  }, [isLog]);
+
+  // useEffect(() => {
+  //   if (window.FB) {
+  //     window.FB.XFBML.parse();
+  //   } else {
+  //     console.log('Button BackOffice not loaded');
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    // Assurez-vous que window.FB et window.FB.XFBML sont définis avant d'appeler parse()
+    if (window.FB && window.FB.XFBML) {
+      window.FB.XFBML.parse();
+    } else {
+      console.log('Button BackOffice not loaded');
+    }
+  }, []);
+
+
+  useEffect(() => {
+    // Vérifier si l'utilisateur est connecté en tant qu'admin
+    const checkAdminStatus = async () => {
+      // Exemple de logique asynchrone pour vérifier les rôles
+      if (isLog) {
+        try {
+          // Ici, vous pouvez effectuer une requête ou une vérification nécessaire pour obtenir les rôles
+          // Exemple: const roles = await fetchRoles();
+          const isAdmin = roles && roles.includes('ROLE_ADMIN');
+          setIsAdmin(isAdmin);
+        } catch (error) {
+          console.error('Erreur lors de la récupération des rôles:', error);
+        }
+      }
+    };
+  
+    // Appeler la fonction de vérification
+    checkAdminStatus();
+  }, [isLog, roles]);
+  
   const handleBackdropClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (e.target === e.currentTarget) {
       closeModal();
@@ -47,21 +98,23 @@ const ModalProfil: React.FC<ModalProfilProps> = ({ closeModal }) => {
             className="w-24 h-24 border-white mb-4"
           />
           <div className="flex flex-col space-y-4 text-xl">
-          {!isLog ? (
-            <Link
-              to="/connexion"
-              className="hover:bg-blue-500 px-4 py-2 rounded-lg hover:text-white transition-colors duration-300"
-              onClick={closeModal}
-            >
-              Profil
-            </Link>
-          ) :   <Link
-          to="/profil"
-          className="hover:bg-blue-500 px-4 py-2 rounded-lg hover:text-white transition-colors duration-300"
-          onClick={closeModal}
-        >
-          Profil
-        </Link>}
+            {!isLog ? (
+              <Link
+                to="/connexion"
+                className="hover:bg-blue-500 px-4 py-2 rounded-lg hover:text-white transition-colors duration-300"
+                onClick={closeModal}
+              >
+                Profil
+              </Link>
+            ) : (
+              <Link
+                to="/profil"
+                className="hover:bg-blue-500 px-4 py-2 rounded-lg hover:text-white transition-colors duration-300"
+                onClick={closeModal}
+              >
+                Profil
+              </Link>
+            )}
             <Link
               to="/parametre"
               className="hover:bg-blue-500 px-4 py-2 rounded-lg hover:text-white transition-colors duration-300"
@@ -71,7 +124,7 @@ const ModalProfil: React.FC<ModalProfilProps> = ({ closeModal }) => {
             </Link>
             <Link
               to="/test-personnalite"
-              className="hover:bg-blue-500 px-4 py-2 rounded-lg hover:text-white transition-colors duration-300"
+              className="hover:bg-blue-500 px-4 py-2 rounded-lg hover:text-white transition-colors durée-300"
               onClick={closeModal}
             >
               Test personnalisé
@@ -86,17 +139,17 @@ const ModalProfil: React.FC<ModalProfilProps> = ({ closeModal }) => {
               </Link>
             )}
             {isLog && (
-              <Link
-              to="/connexion" >
-              <button
-                className="hover:bg-red-600 px-4 py-2 rounded-lg hover:text-white transition-colors duration-300"
-                onClick={() => {
-                  logout();
-                  closeModal();
-                }}
-              >
-                Déconnexion
-              </button></Link>
+              <Link to="/connexion">
+                <button
+                  className="hover:bg-red-600 px-4 py-2 rounded-lg hover:text-white transition-colors duration-300"
+                  onClick={() => {
+                    logout();
+                    closeModal();
+                  }}
+                >
+                  Déconnexion
+                </button>
+              </Link>
             )}
           </div>
           <div className="p-6 rounded shadow-lg bg-blue-900">
@@ -119,3 +172,5 @@ const ModalProfil: React.FC<ModalProfilProps> = ({ closeModal }) => {
 };
 
 export default ModalProfil;
+
+
